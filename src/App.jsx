b22e2895e4271dbbc327abe5bcbd078e3dba5849
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-08-16 12:12:20 +0300

    Projektiin lisätty komponentti Footer ohjeiden mukaan.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+const Footer = () => {
+  return (
+    <>
+      greeting app created by
+      <a href="https://github.com/AnnamariKoo">AnnamariKoo</a>
+    </>
+  );
+};
+
 const Hello = (props) => {
   console.log(props);
   return (
-    <div>
+    <>
       <p>
         Hello {props.name}, you are {props.age} years old.
       </p>
-    </div>
+    </>
   );
 };
 
@@ -18,6 +27,7 @@ const App = () => {
         <h1> Greetings</h1>
         <Hello name="Maya" age={26 + 10} />
         <Hello name={nimi} age={ika} />
+        <Footer />
       </section>
     </>
   );
