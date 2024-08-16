366c9c251128193b46c7c9cbc8cfa302f6ac4bfc
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-08-16 11:08:40 +0300

    Tehtävää jatkettu lisäämällä yksi komponentti sekä props tiedonvälitys komponenttien välille.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old.
+      </p>
+    </div>
+  );
+};
+
 const App = () => {
-  console.log("Hello from komponentti App.");
+  const nimi = "Pekka";
+  const ika = 10;
   return (
     <>
       <section>
-        <p>App-komponentissa on sisältö: Hello World!</p>
+        <h1> Greetings</h1>
+        <Hello name="Maya" age={26 + 10} />
+        <Hello name={nimi} age={ika} />
       </section>
     </>
   );
